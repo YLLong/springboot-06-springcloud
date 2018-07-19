3de2111a4f6bf30113240b8acfa25c517ad77596
yanlinlong <yanlinlung@163.com>
2018-07-19 16:23:46 +0800

    hystrix容错保护 服务降级

---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/akira/hystrix/controller/DcController.java
@@ -0,0 +1,19 @@
+package com.akira.hystrix.controller;
+
+import com.akira.hystrix.service.ConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DcController {
+
+    @Autowired
+    ConsumerService consumerService;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return consumerService.consumer();
+    }
+
+}
---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/akira/hystrix/service/ConsumerService.java
@@ -0,0 +1,23 @@
+package com.akira.hystrix.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ConsumerService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    public String consumer() {
+        return restTemplate.getForObject("http://EUREKA-CLIENT/dc", String.class);
+    }
+
+    public String fallback() {
+        return "fallback";
+    }
+
+}
